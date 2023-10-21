a9ef0aebd7b175b4a1e5e050e8c2b37f157dc407
EE02323 <Enas.elsayed@uranium-corp.com>
2023-10-21 23:32:01 +0200

    react-redux-tolkit

--- app/store.js
+++ app/store.js
@@ -2,13 +2,16 @@ const configureStore = require("@reduxjs/toolkit").configureStore;
 const { getDefaultMiddleware } = require("@reduxjs/toolkit");
 const cackeReducer= require("./feauture/cacke/CackeSlice");
 const iccreanReducer = require("./feauture/Icecream/IcecreamSlice");
+const userReducer = require("./feauture/user/usersSlice");
+
 // const ReduxLogger = require("redux-logger");
 // const logger = ReduxLogger.createLogger();
 
 const store = configureStore({
     reducer:{
         cacke:cackeReducer,
-        icecream:iccreanReducer
+        icecream:iccreanReducer,
+        user:userReducer
     },
     // middleware:(getDefaultMiddle)=>getDefaultMiddle().concat(logger)
 })
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cackeActions = require("./app/feauture/cacke/CackeSlice").CackeAction;
  const icecreamActions= require("./app/feauture/Icecream/IcecreamSlice").icecreamActions;
 const store = require("./app/store");
+const fetchUsers = require("./app/feauture/user/usersSlice").fetchUsers
 console.log("initalstate",store.getState());
 
 const unsubscribe= store.subscribe(()=>
@@ -8,12 +9,14 @@ const unsubscribe= store.subscribe(()=>
 {console.log("updatestate",store.getState())}
 )
 
-store.dispatch(cackeActions.orderd())
-store.dispatch(cackeActions.orderd())
+// store.dispatch(cackeActions.orderd())
+// store.dispatch(cackeActions.orderd())
 // store.dispatch(cackeActions.orderd())
 // store.dispatch(cackeActions.orderd())
 // store.dispatch(cackeActions.reStocked(6))
 // store.dispatch(icecreamActions.orderd())
 // store.dispatch(icecreamActions.restocked(6))
+store.dispatch(fetchUsers())
+store.dispatch(fetchUsers())
+store.dispatch(fetchUsers())
 
-unsubscribe()
